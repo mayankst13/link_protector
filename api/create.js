203c60ee9af292066a598c3dc23d73bde36be1ff
mayankst13 <stmayank92@gmail.com>
2026-02-21 10:24:57 +0530

    create.js

--- api/create.js
+++ api/create.js
@@ -1,24 +1,30 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
 
   if (req.method !== "POST") {
-    return res.status(405).end();
+    return res.status(405).send("Method Not Allowed");
   }
 
-  const { url } = req.body;
+  try {
+    const body = JSON.parse(req.body);
+    const { url } = body;
 
-  if (!url) {
-    return res.status(400).json({ error: "URL required" });
+    if (!url) {
+      return res.status(400).json({ error: "URL required" });
+    }
+
+    const token = jwt.sign(
+      { url },
+      process.env.JWT_SECRET || "supersecretkey123",
+      { expiresIn: "10m" }
+    );
+
+    return res.status(200).json({
+      link: `/l/${token}`
+    });
+
+  } catch (err) {
+    return res.status(500).json({ error: "Server Error" });
   }
-
-  const token = jwt.sign(
-    { url },
-    "supersecretkey123",
-    { expiresIn: "10m" }
-  );
-
-  res.json({
-    link: `/l/${token}`
-  });
 };
